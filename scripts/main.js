957dc15d54c085caea942f5a1d3a4236460de923
Ryohei Ishikawa <88309634+ryoheyyattaze@users.noreply.github.com>
2021-10-14 13:33:37 +0900

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -2,15 +2,14 @@
 const cover = document.querySelector('.mobile_menu__cover');
 const side = document.querySelector('.mobile_menu');
 const links = document.querySelectorAll('.mobile_menu__link');
-const btn = document.querySelector('.header__btn');
+const btn = document.querySelector('.humberger');
 function menuOpen() {
-  const spans = document.querySelectorAll('#span');
+  const spans = document.querySelectorAll('.span');
   spans.forEach(span => {
     span.classList.toggle('menu-open');
   });
-  // cover.classList.toggle('menu-open');
   btn.classList.toggle('menu-open');
-  side.classList.toggle('menu-open')
+  side.classList.toggle('menu-open');
 }
 btn.addEventListener('click', () => {
   menuOpen();
@@ -20,9 +19,24 @@ links.forEach(link => {
     menuOpen();
   });
 });
-// cover.addEventListener('click', () => {
-//   menuOpen();
-// });
+cover.addEventListener('click', () => {
+  menuOpen();
+});
+
+//文字アニメーション
+function TextAnimation(el) {
+  el = document.querySelector(el);
+  const str = el.innerHTML;
+  const strArry = str.trim().split('');
+  const result = strArry.reduce((acc, crr) => {
+    return `${acc}<span class="target char">${crr}</span>`;
+  }, '');
+  el.innerHTML = result;
+}
+
+TextAnimation('.title1');
+TextAnimation('.title2');
+TextAnimation('.title3');
 
 
 //スクロールオブザーバー
@@ -39,7 +53,7 @@ const cb = function(entries, observer) {
 }
 const options = {
   root: null,
-  rootMargin: "0px 0px 0px 0px",
+  rootMargin: "0px 0px 100px 0px",
 }
 const io = new IntersectionObserver(cb, options);
 targets.forEach(target => {
@@ -47,20 +61,3 @@ targets.forEach(target => {
 });
 
 
-//文字アニメーション
-class TextAniamtion {
-  constructor(el) {
-    this.el = document.querySelector(el);
-    this.chars = this.el.innerHTML.trim().split('');
-    this.el.innerHTML = this._tagText();
-  }
-  _tagText() {
-    return this.chars.reduce((acc, crr) => {
-      return `${acc}<span class="char">${crr}</span>`;
-    }, '');
-  }
-}
-
-const str1 = new TextAniamtion('.concept__title');
-const str2 = new TextAniamtion('.menues__title');
-const str3 = new TextAniamtion('.store__title');
